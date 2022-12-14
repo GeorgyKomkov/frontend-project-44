c38b85ac594d40189af7b9bc97ac4d9ff40c248f
Gosha-PC <Komkov.g.o@yandex.ru>
2022-12-14 17:30:47 +0300

    поправил файлы после теста

--- src/games/calc.js
+++ src/games/calc.js
@@ -4,7 +4,6 @@ import run from '../index.js';
 
 const description = 'What is the result of the expression?';
 const operators = ['+', '-', '*'];
-
 const calculate = (num1, num2, operator) => {
   if (operator === '+') {
     return num1 + num2;
@@ -14,17 +13,14 @@ const calculate = (num1, num2, operator) => {
     return num1 * num2;
   }
 };
-
 const generateRound = () => {
   const number1 = getRandomNumber(0, 10);
   const number2 = getRandomNumber(0, 10);
   const operator = operators[getRandomIndex(operators)];
-
   const question = `${number1} ${operator} ${number2}`;
   const correctAnswer = calculate(number1, number2, operator).toString();
   return [question, correctAnswer];
 };
-
 export default () => {
   run(description, generateRound);
 };
