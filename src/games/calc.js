a4d8b76ea58e6f52c8fefffb15f751874eb5c2a5
Gosha-PC <Komkov.g.o@yandex.ru>
2022-12-14 19:52:28 +0300

    отредактировал файлы после теста

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
-import { getRandomNumber, getRandomIndex } from "../randomValues.js";
-import run from "../index.js";
+import { getRandomNumber, getRandomIndex } from '../randomValues.js';
+import run from '../index.js';
 
-const description = "What is the result of the expression?";
-const operators = ["+", "-", "*"];
+const description = 'What is the result of the expression?';
+const operators = ['+', '-', '*'];
 const calculate = (num1, num2, operator) => {
-  if (operator === "+") return num1 + num2;
-  if (operator === "-") return num1 - num2;
-  if (operator === "*") return num1 * num2;
+  if (operator === '+') return num1 + num2;
+  if (operator === '-') return num1 - num2;
+  if (operator === '*') return num1 * num2;
+  return 'Error'
 };
 const generateRound = () => {
   const number1 = getRandomNumber(0, 10);
--- src/index.js
+++ src/index.js
@@ -10,13 +10,11 @@ export default (description, generateRound) => {
     console.log(`Question: ${question}`);
     const playerAnswer = readlineSync.question('Your answer: ');
     if (playerAnswer !== correctAnswer) {
-      console.log(
-        `'${playerAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`
-      );
+      console.log(`'${playerAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
       console.log(`Let's try again, ${playerName}!`);
       return;
-    };
+    }
     console.log('Correct!');
-  };
+  }
   console.log(`Congratulations, ${playerName}!`);
 };
