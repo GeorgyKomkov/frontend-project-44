ae9693d3ad861e64862249a56eab0e2ce8a1654b
Gosha-PC <Komkov.g.o@yandex.ru>
2022-12-14 19:55:37 +0300

    отредактировал файлы после теста

--- src/games/calc.js
+++ src/games/calc.js
@@ -8,7 +8,7 @@ const calculate = (num1, num2, operator) => {
   if (operator === '+') return num1 + num2;
   if (operator === '-') return num1 - num2;
   if (operator === '*') return num1 * num2;
-  return 'Error'
+  return 'Error';
 };
 const generateRound = () => {
   const number1 = getRandomNumber(0, 10);
