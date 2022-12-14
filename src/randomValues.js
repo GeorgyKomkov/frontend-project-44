a43cb4ba4086a38205a9abaf1e27c556285aed43
Gosha-PC <Komkov.g.o@yandex.ru>
2022-12-14 16:30:40 +0300

    перенастроил eslint

--- src/randomValues.js
+++ src/randomValues.js
@@ -1,3 +1,4 @@
+ 
 const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 const getRandomIndex = (arr) => Math.floor(Math.random() * arr.length);
 export { getRandomNumber, getRandomIndex };
