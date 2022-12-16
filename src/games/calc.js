59a9a551f12769caea083199ae10b79f05038e8c
Gosha-PC <Komkov.g.o@yandex.ru>
2022-12-16 13:38:21 +0300

    провел рефакторинг: улучшил оформление, добавил описание в package.json, корректно изменил именование функций, избавился от  магического значения в цикле  файла index.js, переименовал файл с вспомогательными функциями в ultis.js, в файле calc.js  изменил условную конструцкию на switch

--- src/games/calc.js
+++ src/games/calc.js
@@ -9,16 +9,16 @@ const calculate = (number1, number2, operator) => {
   // if (operator === '-') return number1 - number2;
   // if (operator === '*') return number1 * number2;
   // return 'Error';
-    switch (operator) {
-      case '+':
-        return number1 + number2;
-      case '-':
-        return number1 - number2;
-      case '*':
-        return number1 * number2;
-      default:
-        throw new Error(`There is no such operator  '${operator}'!`);
-    }
+  switch (operator){
+    case '+':
+      return number1 + number2;
+    case '-':
+      return number1 - number2;
+    case '*':
+      return number1 * number2;
+    default:
+      throw new Error(`There is no such operator  '${operator}'!`) 
+  }
 };
 const generateRound = () => {
   const number1 = getRandomNumber(0, 10);
