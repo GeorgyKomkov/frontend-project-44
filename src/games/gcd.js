3e736d9e2c94afc95d40593dd2fad8babb39bfaf
Gosha-PC <Komkov.g.o@yandex.ru>
2022-12-19 13:49:04 +0300

    Изменил название функции на более корректное в файле brain-games.js, удалил закомментированный код, в файле gcd.js упростил функцию gcd, убрал "магические значения", добавив константы, отредактировал общий вид модулей, испаривл опечатки

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -4,7 +4,7 @@ import run from '../index.js';
 
 const description = 'Find the greatest common divisor of given numbers';
 
-const gcd = (number1, number2) => (number2 === 0 ? number1 : gcd(number2,number1 % number2));
+const gcd = (number1, number2) => (number2 === 0 ? number1 : gcd(number2, number1 % number2));
 
 const generateRound = () => {
   const number1 = getRandomNumber(1, 100);
